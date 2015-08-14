65e3ea4737908ff94e083d6309f0ec66f4c6e93b
Calvin Cheng <linchuan.cheng@gmail.com>
2015-08-14 15:48:26 +0800

    first commit

--- SearchApp/ViewController.swift
+++ SearchApp/ViewController.swift
@@ -6,13 +6,31 @@
 //  Copyright © 2015 Hello HQ Pte. Ltd. All rights reserved.
 //
 
+// High-level principle for search logic
+
+// We filter the `data` array and if it matches the text we create a new array called `filtered` which holds the text which matches the text in the searchbar.
+
+// Depending on the `searchActive` we return either the `filtered` or the actual `data` in the `numberOfRowsInSection` and `cellForRowAtIndexPath`
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchActive: Bool = false
+    var data = ["San Francisco", "New York", "San Jose", "Chicago", "Los Angeles", "Austin", "Seattle"]
+    var filtered:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set up
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,66 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Conform to UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if(searchActive) {
+            return filtered.count
+        }
+        
+        return data.count;
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        if(searchActive){
+            cell.textLabel?.text = filtered[indexPath.row]
+        } else {
+            cell.textLabel?.text = data[indexPath.row];
+        }
+
+        return cell
+        
+    }
+    
+    // Search logic
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchActive = true
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchActive = false
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchActive = false
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchActive = false
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filtered = data.filter({ (text) -> Bool in
+            let tmp: NSString = text
+            let range = tmp.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
+            return range.location != NSNotFound
+        })
+        
+        if(filtered.count == 0){
+            searchActive = false;
+        } else {
+            searchActive = true;
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    
 
 }
 
