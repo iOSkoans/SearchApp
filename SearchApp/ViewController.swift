5385ea8e054bc4f65b1deec40cc967a2009a7412
Calvin Cheng <linchuan.cheng@gmail.com>
2015-08-14 15:06:09 +0800

    Initial Commit

---
+++ SearchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchApp
+//
+//  Created by Calvin Cheng on 14/8/15.
+//  Copyright © 2015 Hello HQ Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
